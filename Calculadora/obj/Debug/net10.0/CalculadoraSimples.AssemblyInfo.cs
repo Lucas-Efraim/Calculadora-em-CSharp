b72d63e80b8b9c5d27546a523cf5ea83f8245cbe
Lucas Efraim <lucassaldanha.dev@gmail.com>
2026-04-03 13:07:41 -0300

    Atualização

--- Calculadora/obj/Debug/net10.0/CalculadoraSimples.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net10.0/CalculadoraSimples.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraSimples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9838ecf229a725309cca392913810bb391a405b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+955156cc75759a05e0b7364898d5932b0901c67e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraSimples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraSimples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
